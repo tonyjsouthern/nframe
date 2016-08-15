8d8ecd25c9c9fa2967cf7b8219d2fe964f4bf4b5
TJ Southern <tonyjsouthern@gmail.com>
2016-08-15 19:46:21 -0400

    added iPhone 6s

--- src/js/phones.js
+++ src/js/phones.js
@@ -2,6 +2,7 @@ var phones = [
   // nexus 5
   {
     "id": "0",
+    "name": "Nexus 5",
     "model": "Nexus-5",
     "url": "http://imgur.com/P26cZUt.png",
     "thumbUrl": "http://i.imgur.com/IWePC3c.png"
@@ -9,6 +10,7 @@ var phones = [
   // nexus 6
   {
     "id": "1",
+    "name": "Nexus 6",
     "model": "Nexus-6",
     "url": "http://i.imgur.com/28gxsIz.png",
     "thumbUrl": "http://i.imgur.com/wDz8oXE.png"
@@ -16,6 +18,7 @@ var phones = [
   // Nexus 6p
   {
     "id": "2",
+    "name": "Nexus 6P",
     "model": "Nexus-6p",
     "url": "http://imgur.com/ZJ7Irn2.png",
     "thumbUrl": "http://i.imgur.com/lOVgHTp.png"
@@ -23,9 +26,17 @@ var phones = [
   // nexus 5x
   {
     "id": "3",
+    "name": "Nexus 5x",
     "model": "Nexus-5x",
     "url": "http://imgur.com/PlOhVxI.png",
     "thumbUrl": "http://i.imgur.com/3vKcT7H.png"
-  }];
+  },
+{
+  "id": "4",
+  "name": "iPhone 6s",
+  "model": "iPhone-6s",
+  "url": "http://i.imgur.com/uRpSlV1.png",
+  "thumbUrl": "http://i.imgur.com/NQFksq2.png"
+}];
 
   export { phones }
