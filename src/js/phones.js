be35bc5199445d686d8754fa96c86a21a12270e7
TJ Southern <tonyjsouthern@gmail.com>
2016-08-18 11:05:39 -0400

    changed phone urls

--- src/js/phones.js
+++ src/js/phones.js
@@ -4,23 +4,23 @@ var phones = [
     "id": "0",
     "name": "Nexus 5",
     "model": "Nexus-5",
-    "url": "/../app/images/Nexus5/Nexus5.png",
-    "thumbUrl": "http://i.imgur.com/IWePC3c.png"
+    "url": "https://github.com/tonyjsouthern/nframe/blob/master/app/images/Nexus5/Nexus5.png?raw=true",
+    "thumbUrl": "https://github.com/tonyjsouthern/nframe/blob/master/app/images/Nexus5/nexus5thumb.png?raw=true"
   },
   // nexus 6
   {
     "id": "1",
     "name": "Nexus 6",
     "model": "Nexus-6",
-    "url": "http://i.imgur.com/28gxsIz.png",
-    "thumbUrl": "http://i.imgur.com/wDz8oXE.png"
+    "url": "https://github.com/tonyjsouthern/nframe/blob/master/app/images/Nexus6/nexus6.png?raw=true",
+    "thumbUrl": "https://github.com/tonyjsouthern/nframe/blob/master/app/images/Nexus6/nexus6thumb.png?raw=true"
   },
   // Nexus 6p
   {
     "id": "2",
     "name": "Nexus 6P",
     "model": "Nexus-6p",
-    "url": "http://imgur.com/ZJ7Irn2.png",
+    "url": "https://github.com/tonyjsouthern/nframe/blob/master/app/images/Nexus6p/Nexus6p.png?raw=true",
     "thumbUrl": "http://i.imgur.com/lOVgHTp.png"
   },
   // nexus 5x
@@ -28,7 +28,7 @@ var phones = [
     "id": "3",
     "name": "Nexus 5x",
     "model": "Nexus-5x",
-    "url": "http://imgur.com/PlOhVxI.png",
+    "url": "https://github.com/tonyjsouthern/nframe/blob/master/app/images/Nexus5x/Nexus5x.png?raw=true",
     "thumbUrl": "http://i.imgur.com/3vKcT7H.png"
   },
   // LG G3
@@ -36,16 +36,16 @@ var phones = [
     "id": "4",
     "name": "LG G3",
     "model": "LGG3",
-    "url": "http://i.imgur.com/j3vvOfM.png",
-    "thumbUrl": "http://i.imgur.com/JbilZyE.png"
+    "url": "https://github.com/tonyjsouthern/nframe/blob/master/app/images/LGG3/LGG3.png?raw=true",
+    "thumbUrl": "https://github.com/tonyjsouthern/nframe/blob/master/app/images/LGG3/LGG3thumb.png?raw=true"
   },
   // iPhone 6s/6
   {
     "id": "5",
     "name": "iPhone 6s",
     "model": "iPhone-6s",
-    "url": "http://i.imgur.com/uRpSlV1.png",
-    "thumbUrl": "http://i.imgur.com/NQFksq2.png"
+    "url": "https://github.com/tonyjsouthern/nframe/blob/master/app/images/iPhone6/iPhone6.png?raw=true",
+    "thumbUrl": "https://github.com/tonyjsouthern/nframe/blob/master/app/images/iPhone6/iPhone6thumb.png?raw=true"
   }];
 
   export { phones }
