26da73b311068039639f2370b49b850ee06170f5
TJ Southern <tonyjsouthern@gmail.com>
2016-08-22 00:26:25 -0400

    added 3 new phones and remade old phone images

--- src/js/phones.js
+++ src/js/phones.js
@@ -4,48 +4,72 @@ var phones = [
     "id": "0",
     "name": "Nexus 5",
     "model": "Nexus-5",
-    "url": "./images/Nexus5/Nexus5.png",
-    "thumbUrl": "./images/Nexus5/nexus5thumb.png"
+    "url": "./images/nexus5/nexus5.png",
+    "thumbUrl": "./images/nexus5/nexus5thumb.png"
   },
   // nexus 6
   {
     "id": "1",
     "name": "Nexus 6",
     "model": "Nexus-6",
-    "url": "./images/Nexus6/Nexus6.png",
-    "thumbUrl": "./images/Nexus6/nexus6thumb.png"
+    "url": "./images/nexus6/nexus6.png",
+    "thumbUrl": "./images/nexus6/nexus6thumb.png"
   },
   // Nexus 6p
   {
     "id": "2",
     "name": "Nexus 6P",
     "model": "Nexus-6p",
-    "url": "./images/Nexus6p/Nexus6p.png",
-    "thumbUrl": "./images/Nexus6p/Nexus6pthumb.png"
+    "url": "./images/nexus6p/nexus6p.png",
+    "thumbUrl": "./images/nexus6p/nexus6pthumb.png"
   },
   // nexus 5x
   {
     "id": "3",
     "name": "Nexus 5x",
     "model": "Nexus-5x",
-    "url": "./images/Nexus5x/Nexus5x.png",
-    "thumbUrl": "./images/Nexus5x/Nexus5xthumb.png"
+    "url": "./images/nexus5x/nexus5x.png",
+    "thumbUrl": "./images/nexus5x/nexus5xthumb.png"
   },
   // LG G3
   {
     "id": "4",
     "name": "LG G3",
     "model": "LGG3",
-    "url": "./images/LGG3/LGG3.png",
-    "thumbUrl": "./images/LGG3/LGG3thumb.png"
+    "url": "./images/lgg3/lgg3.png",
+    "thumbUrl": "./images/lgg3/lgg3thumb.png"
+  },
+  // Galaxy S6
+  {
+    "id": "5",
+    "name": "Galaxy S6",
+    "model": "Galaxy-S6",
+    "url": "./images/galaxys6/galaxys6.png",
+    "thumbUrl": "./images/galaxys6/galaxys6thumb.png"
+  },
+  // Galaxy S7
+  {
+    "id": "6",
+    "name": "Galaxy S7",
+    "model": "Galaxy-S7",
+    "url": "./images/galaxys7/galaxys7.png",
+    "thumbUrl": "./images/galaxys7/galaxys7thumb.png"
+  },
+  // Note 5
+  {
+    "id": "7",
+    "name": "Note 5",
+    "model": "Note-5",
+    "url": "./images/note5/note5.png",
+    "thumbUrl": "./images/note5/note5thumb.png"
   },
   // iPhone 6s/6
   {
-    "id": "5",
+    "id": "8",
     "name": "iPhone 6s",
     "model": "iPhone-6s",
-    "url": "./images/iPhone6/iphone6.png",
-    "thumbUrl": "./images/iPhone6/iPhone6thumb.png"
+    "url": "./images/iphone6/iphone6white.png",
+    "thumbUrl": "./images/iphone6/iphone6whitethumb.png"
   }];
 
   export { phones }
